e4d19e4bdb47f7e0dabfa79f4ce49099532b204a
John Gutierrez Gutierrez <jegutierrez@DOMAIN.LOCAL>
2025-04-09 13:14:17 -0500

    feat(post): crear estructura HTML para el formulario de agregar post

--- Frontend/src/app/features/post/components/add-edit-post/add-edit-post.component.ts
+++ Frontend/src/app/features/post/components/add-edit-post/add-edit-post.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-add-edit-post',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './add-edit-post.component.html',
   styleUrl: './add-edit-post.component.css'
 })
