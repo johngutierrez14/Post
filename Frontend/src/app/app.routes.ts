c7c454c0b467a0431ba9384845a108c28fa83538
John Gutierrez Gutierrez <jegutierrez@DOMAIN.LOCAL>
2025-04-09 13:53:38 -0500

    feat: agregar componente de login con estructura base y formulario en HTML

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -3,10 +3,14 @@ import { Routes } from '@angular/router';
 //Components
 import { PostTableComponent } from './features/post/components/post-table/post-table.component';
 import { AddEditPostComponent } from './features/post/components/add-edit-post/add-edit-post.component';
+import { LoginComponent } from './features/auth/components/login/login.component';
+import { RegisterComponent } from './features/auth/components/register/register.component';
 
 
 export const routes: Routes = [
-    {path:'', component: PostTableComponent},
+    {path:'', component: LoginComponent},
+    {path:'login', component: LoginComponent},
+    {path:'register', component: RegisterComponent},
     {path:'add', component: AddEditPostComponent},
     {path:'edit/:id', component: AddEditPostComponent},
     {path:'**', redirectTo: '', pathMatch:'full'} 
